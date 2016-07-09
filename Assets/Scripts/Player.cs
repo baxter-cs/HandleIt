d0b860e64a279c46dda20db9e9f57107ec4de740
halstedlarsson <halsted.larsson@gmail.com>
2016-07-09 14:11:30 -0400

    initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	public PlayerCharacter playerCharater;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void Update () {
+		if (playerCharater != null && playerCharater.IsAlive()) {
+
+			playerCharater.Move (GetInputDir(), IsPressingSprintModifierKey());
+
+			if (Input.GetButtonDown("Jump")){
+				PressedJumpButton();
+			}
+		}
+	}
+	
+	void PressedJumpButton(){
+		playerCharater.Jump();
+	}
+
+	private Vector3 GetInputDir(){
+		float x = Input.GetAxisRaw ("Horizontal");
+		float y = 0;
+		float z = Input.GetAxisRaw ("Vertical");
+		return new Vector3(x,y,z);
+	}
+
+	private bool IsPressingSprintModifierKey() {
+		return Input.GetButton ("Sprint");
+	}
+}
---
+++ Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCharacter : MonoBehaviour {
+
+	Rigidbody phys;
+	public float jumpForce = 1.0f;
+	public float walkingSpeed = 1.0f;
+	public float runningSpeed = 2.0f;
+	public static int staminaCap = 100;
+	public int stamina = staminaCap;
+	private bool isSprinting;
+
+	// Use this for initialization
+	void Start () {
+		phys = GetComponent<Rigidbody> ();
+	}
+
+	void Update () {
+
+	}
+
+	public void Move(Vector3 dir, bool sprinting) {
+		Vector3 jumpVel = Vector3.up * phys.velocity.y;
+		phys.velocity = UpdateSpeed (sprinting) * dir + jumpVel;
+	}
+
+	public void Jump() {
+		phys.AddForce (Vector3.up * jumpForce, ForceMode.Impulse);
+	}
+
+	private float UpdateSpeed (bool sprinting) {
+		if (sprinting && (stamina > 0)) {
+			stamina -= 3;
+			isSprinting = true;
+			return runningSpeed;
+		} else {
+			stamina += stamina < staminaCap ? 1 : 0;
+			isSprinting = false;
+			return walkingSpeed;
+		}
+	}
+
+	public bool IsSprinting() {
+		return isSprinting;
+	}
+
+	public bool IsAlive() {
+		return true;
+	}
+}
